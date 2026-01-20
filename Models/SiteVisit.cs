55f28cad4a382dacd852d2abc730d3507285f521
JIBIN21-Dev <jibubiju751@gmail.com>
2026-01-20 09:27:06 +0530

    Fix SiteVisit VisitDate type

--- Models/SiteVisit.cs
+++ Models/SiteVisit.cs
@@ -3,7 +3,8 @@
     public class SiteVisit
     {
         public int Id { get; set; }
-        public DateTime VisitDate { get; set; }
+        public DateTime VisitDate { get; set; } = DateTime.UtcNow.Date;
+
         public int VisitCount { get; set; } = 1;
     }
 }
